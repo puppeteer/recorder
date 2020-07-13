e6c351fe1efcad6e92d85c2ceee412726ddb7e4a
Jan Scheffler <janscheffler@chromium.org>
2020-07-13 14:53:07 +0200

    Move jest env declaration to top of file

--- test/dom-helpers.spec.ts
+++ test/dom-helpers.spec.ts
@@ -1,4 +1,8 @@
 /**
+ * @jest-environment jsdom
+ */
+
+/**
  * Copyright 2020 Google Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,10 +16,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-
-/**
- * @jest-environment jsdom
  */
 
 import { isSubmitButton, getSelector } from '../src/injected/dom-helpers';
